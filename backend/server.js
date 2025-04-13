ae048b780b876750dbaad940d1f4736cde73353d
Agaditya1000 <agaditya1000@gmail.com>
2025-04-13 20:40:39 +0530

    4th

--- backend/server.js
+++ backend/server.js
@@ -42,4 +42,20 @@ app.use((err, req, res, next) => {
   });
 });
 
+
+app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    next();
+});
+
 app.listen(port, () => console.log(`Server started on PORT:${port}`));
